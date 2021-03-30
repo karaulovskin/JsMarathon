67fa569e632617de181b01f88fa5c5a32a0e29c2
Karaulovskin <karaulovskin@yandex.ru>
2021-03-30 18:09:32 +0300

    work on bugs

--- src/main.js
+++ src/main.js
@@ -8,24 +8,21 @@ const button = document.getElementById('selectALetter');
 button.addEventListener('click', getLetter);
 
 function getRow(firstRow, secondRow, letter) {
-    let resultFirstRow = 0;
-    let resultSecondRow = 0;
-
-    function getNumber(counter) {
-        let result = 0;
-        for (let i = 0; i < counter.length; i++) {
-            if (counter.charAt(i) !== letter) {
-                continue;
-            }
-            result++;
-        }
-        return result
-    }
-
-    resultFirstRow = getNumber(firstRow);
-    resultSecondRow = getNumber(secondRow);
+    let resultFirstRow = getNumber(firstRow, letter);
+    let resultSecondRow = getNumber(secondRow, letter);
 
     alert((resultFirstRow > resultSecondRow) ? firstRow : secondRow);
+}
+
+function getNumber(counter, letter) {
+    let result = 0;
+    for (let i = 0; i < counter.length; i++) {
+        if (counter.charAt(i) !== letter) {
+            continue;
+        }
+        result++;
+    }
+    return result
 }
 
 function getLetter() {
